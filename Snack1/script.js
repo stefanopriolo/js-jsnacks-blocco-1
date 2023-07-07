dfa45941f6e6379f82a7bbc0038d095a98f4e025
stefanopriolo <stefanojpriolo@gmail.com>
2023-07-07 18:14:38 +0200

    fix

--- Snack1/script.js
+++ Snack1/script.js
@@ -1,8 +1,10 @@
 const nameid = ["Stefano", "Demetrio", "Totò", "Simone", "Vittoria"];
 const surname = ["Montalto", "Priolo", "Agostino", "Calabrò", "Foti"];
 
-console.log(
-  nameid[Math.floor(Math.random() * nameid.length)] +
+for (i = 0; i < 10; i++) {
+  const guestList =
+    nameid[Math.floor(Math.random() * nameid.length)] +
     " " +
-    surname[Math.floor(Math.random() * surname.length)]
-);
+    surname[Math.floor(Math.random() * surname.length)];
+  console.log(guestList);
+}
